042847bfd636505c5c5e7548895e40b0091e0d2e
Gabriel Souza <gabrielsouza65115@gmail.com>
2025-12-22 09:41:24 -0300

    fix: Remove imagens inutilizadas e comenta códigos inutilizados

--- src/components/interactives/WhatsAppAnimated.jsx
+++ src/components/interactives/WhatsAppAnimated.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import content from '../../content/content'
 import { getWhatsappLink } from '../util/WhatsappLink'
-import contentLp01 from '../../content/contentLp01'
 
 export default function WhatsappAnimated() {
   const [isModalOpen, setIsModalOpen] = useState(false)
@@ -69,7 +68,7 @@ export default function WhatsappAnimated() {
           body: JSON.stringify({
             nome,
             email: telefone,
-            origem: contentLp01.origem,
+            origem: content.origem,
           }),
         }
       )
